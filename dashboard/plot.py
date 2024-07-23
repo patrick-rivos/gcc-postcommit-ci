d938f6207920056928cb2e8f7b7422f0aedfa4be
Kevin Lee <kevinl@rivosinc.com>
2024-07-23 12:35:46 -0700

    feat: Add pre-commit-hooks for better lint

--- dashboard/plot.py
+++ dashboard/plot.py
@@ -6,38 +6,62 @@ import plotly.express as px
 
 def clean(old_df: pd.DataFrame):
     df = old_df.replace(" multilib", "", regex=True)
-    df.replace("rv32gc_zba_zbb_zbc_zbs ilp32d medlow", "rv32 Bitmanip", regex=True, inplace=True)
-    df.replace("rv64gc_zba_zbb_zbc_zbs lp64d medlow", "rv64 Bitmanip", regex=True, inplace=True)
-    df.replace("rv64imafdcv_zicond_zawrs_zbc_zvkng_zvksg_zvbb_zvbc_zicsr_zba_zbb_zbs_zicbom_zicbop_zicboz_zfhmin_zkt lp64d medlow", "rv64 RVA", regex=True, inplace=True)
-    df.replace("rv64gcv_zvbb_zvbc_zvkg_zvkn_zvknc_zvkned_zvkng_zvknha_zvknhb_zvks_zvksc_zvksed_zvksg_zvksh_zvkt lp64d medlow", "rv64 Vector Crypto", regex=True, inplace=True)
-    df.replace("rv32gcv_zvbb_zvbc_zvkg_zvkn_zvknc_zvkned_zvkng_zvknha_zvknhb_zvks_zvksc_zvksed_zvksg_zvksh_zvkt ilp32d medlow", "rv32 Vector Crypto", regex=True, inplace=True)
+    df.replace(
+        "rv32gc_zba_zbb_zbc_zbs ilp32d medlow",
+        "rv32 Bitmanip",
+        regex=True,
+        inplace=True,
+    )
+    df.replace(
+        "rv64gc_zba_zbb_zbc_zbs lp64d medlow", "rv64 Bitmanip", regex=True, inplace=True
+    )
+    df.replace(
+        "rv64imafdcv_zicond_zawrs_zbc_zvkng_zvksg_zvbb_zvbc_zicsr_zba_zbb_zbs_zicbom_zicbop_zicboz_zfhmin_zkt lp64d medlow",
+        "rv64 RVA",
+        regex=True,
+        inplace=True,
+    )
+    df.replace(
+        "rv64gcv_zvbb_zvbc_zvkg_zvkn_zvknc_zvkned_zvkng_zvknha_zvknhb_zvks_zvksc_zvksed_zvksg_zvksh_zvkt lp64d medlow",
+        "rv64 Vector Crypto",
+        regex=True,
+        inplace=True,
+    )
+    df.replace(
+        "rv32gcv_zvbb_zvbc_zvkg_zvkn_zvknc_zvkned_zvkng_zvknha_zvknhb_zvks_zvksc_zvksed_zvksg_zvksh_zvkt ilp32d medlow",
+        "rv32 Vector Crypto",
+        regex=True,
+        inplace=True,
+    )
     df.replace(" medlow", "", regex=True, inplace=True)
 
-    df['libname'] = old_df['target'].str.replace(" multilib", "").replace(" medlow", "")
+    df["libname"] = old_df["target"].str.replace(" multilib", "").replace(" medlow", "")
 
-    df['libc-target'] = df['libc'] + "-" + df["target"].str.strip()
+    df["libc-target"] = df["libc"] + "-" + df["target"].str.strip()
 
-    df['hash_timestamp'] = pd.to_datetime(df['hash_timestamp'], utc=True)
+    df["hash_timestamp"] = pd.to_datetime(df["hash_timestamp"], utc=True)
 
-    df.sort_values(by='hash_timestamp', inplace=True)
+    df.sort_values(by="hash_timestamp", inplace=True)
 
     # Ignore hash where we tried to update the rva23 profile and failed.
-    df = df.drop(df[df["gcc_hash"] == '0f1727e25f4440bce00271b1e9cf7e7f9125acf0'].index)
+    df = df.drop(df[df["gcc_hash"] == "0f1727e25f4440bce00271b1e9cf7e7f9125acf0"].index)
 
     # If there hasn't been a new datapoint in 3 months we've stopped testing this target.
     # Remove it from the dashboard.
     for libc_target in set(df["libc-target"]):
-      max_timestamp = max(df[df["libc-target"] == libc_target]['hash_timestamp'])
-      if max_timestamp.date() < datetime.date.today() - datetime.timedelta(days=3 * 365/12):
-        df = df[df["libc-target"] != libc_target]
+        max_timestamp = max(df[df["libc-target"] == libc_target]["hash_timestamp"])
+        if max_timestamp.date() < datetime.date.today() - datetime.timedelta(
+            days=3 * 365 / 12
+        ):
+            df = df[df["libc-target"] != libc_target]
 
     return df
 
 
 def plot_cumulative_state(df: pd.DataFrame, outfile: str, wrap: int = 3):
-    range_max = max(df['hash_timestamp']) + datetime.timedelta(hours=6)
+    range_max = max(df["hash_timestamp"]) + datetime.timedelta(hours=6)
 
-    range_min = max(df['hash_timestamp']) - datetime.timedelta(days=9)
+    range_min = max(df["hash_timestamp"]) - datetime.timedelta(days=9)
 
     fig = px.line(
         df,
@@ -48,27 +72,27 @@ def plot_cumulative_state(df: pd.DataFrame, outfile: str, wrap: int = 3):
             "unique_fails": "unique_fails",
         },
         markers=True,
-        hover_data=["gcc_hash","libname"],
+        hover_data=["gcc_hash", "libname"],
         category_orders={
-            'libc-target': sorted(list(set(df["libc-target"].to_list()))),
-                         },
-        facet_col='libc-target',
+            "libc-target": sorted(list(set(df["libc-target"].to_list()))),
+        },
+        facet_col="libc-target",
         facet_col_wrap=wrap,
-        facet_row_spacing=0.04, # default is 0.07 when facet_col_wrap is used
-        facet_col_spacing=0, # default is 0.03
-        color='tool',
-        symbol='tool',
-        title=f"Unique/total failures per hash<br><sup>Data sourced from <a href=\"https://github.com/patrick-rivos/gcc-postcommit-ci\">gcc-postcommit-ci</a> and older data from <a href=\"https://github.com/patrick-rivos/riscv-gnu-toolchain\">riscv-gnu-toolchain</a></sup>",
+        facet_row_spacing=0.04,  # default is 0.07 when facet_col_wrap is used
+        facet_col_spacing=0,  # default is 0.03
+        color="tool",
+        symbol="tool",
+        title=f'Unique/total failures per hash<br><sup>Data sourced from <a href="https://github.com/patrick-rivos/gcc-postcommit-ci">gcc-postcommit-ci</a> and older data from <a href="https://github.com/patrick-rivos/riscv-gnu-toolchain">riscv-gnu-toolchain</a></sup>',
         range_x=[range_min, range_max],
-        range_y=[-5, max(df[df['hash_timestamp'] > range_min]['total_fails']) + 5],
+        range_y=[-5, max(df[df["hash_timestamp"] > range_min]["total_fails"]) + 5],
     )
 
-    fig.write_html(outfile, include_plotlyjs='cdn')
+    fig.write_html(outfile, include_plotlyjs="cdn")
 
 
 if __name__ == "__main__":
-    linux_data = pd.read_csv('linux.csv')
-    newlib_data = pd.read_csv('newlib.csv')
+    linux_data = pd.read_csv("linux.csv")
+    newlib_data = pd.read_csv("newlib.csv")
     raw_data = pd.concat([newlib_data, linux_data])
     all_data = clean(raw_data)
 
@@ -77,7 +101,9 @@ if __name__ == "__main__":
 
     os.makedirs(f"site", exist_ok=True)
     main_dashboard_data = all_data[~all_data["libc-target"].str.contains("gcv_zvl")]
-    main_dashboard_data = main_dashboard_data[~main_dashboard_data["libc-target"].str.contains("gcv_zve")]
+    main_dashboard_data = main_dashboard_data[
+        ~main_dashboard_data["libc-target"].str.contains("gcv_zve")
+    ]
     plot_cumulative_state(main_dashboard_data, "site/index.html")
 
     gcv = all_data[all_data["libc-target"].str.contains("gcv")]
